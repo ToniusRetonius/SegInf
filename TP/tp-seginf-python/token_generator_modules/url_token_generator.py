39badb24636546d3c8617104206c65439e2dbab1
ToniusRetonius <toto.melli074@gmail.com>
2025-12-14 15:58:08 -0300

    TP

--- TP/tp-seginf-python/token_generator_modules/url_token_generator.py
+++
@@ -1,7 +0,0 @@
-from criptography_modules.sign_modules.token_sign_module import sign_message
-from url_creator import create_url
-def create_url_token():
-    url = create_url()
-    print("\nURL generada:")
-    print(url)
-    return url
